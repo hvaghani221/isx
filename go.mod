03e9b7c40d9f8b4fa4e9fa3a70375a58113d3a01
Harshit Vaghani <harshitvaghani221@gmail.com>
2024-03-19 17:25:03 +0530

    refactored code

--- go.mod
+++ go.mod
@@ -12,11 +12,12 @@ require (
 	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
+	github.com/lrstanley/bubblezone v0.0.0-20240125042004-b7bafc493195 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/ansi v0.0.0-20211031195517-c9f0611b6c70 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
@@ -24,5 +25,5 @@ require (
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/term v0.6.0 // indirect
-	golang.org/x/text v0.3.8 // indirect
+	golang.org/x/text v0.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,186 +1,27 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"os"
-	"strings"
+	"runtime/pprof"
 
-	"github.com/charmbracelet/bubbles/key"
-	"github.com/charmbracelet/bubbles/textinput"
-	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
+
+	"github.com/hvaghani221/isx/shell"
+	"github.com/hvaghani221/isx/ui"
 )
 
-type model struct {
-	self        *tea.Program
-	sh          *shell
-	input       strings.Builder
-	viewport    viewport.Model
-	cmdInput    textinput.Model
-	ctx         context.Context
-	cancelShell func()
-}
-
-var viewportKeyMap = viewport.KeyMap{
-	PageDown: key.NewBinding(
-		key.WithKeys("pgdown"),
-		key.WithHelp("pgdn", "page down"),
-	),
-	PageUp: key.NewBinding(
-		key.WithKeys("pgup"),
-		key.WithHelp("pgup", "page up"),
-	),
-	HalfPageUp: key.NewBinding(
-		key.WithKeys("ctrl+u"),
-		key.WithHelp("ctrl+u", "½ page up"),
-	),
-	HalfPageDown: key.NewBinding(
-		key.WithKeys("ctrl+d"),
-		key.WithHelp("ctrl+d", "½ page down"),
-	),
-	Up: key.NewBinding(
-		key.WithKeys("up", "ctrl+k"),
-		key.WithHelp("↑/k", "up"),
-	),
-	Down: key.NewBinding(
-		key.WithKeys("down", "ctrl+j"),
-		key.WithHelp("↓/j", "down"),
-	),
-}
-
-func newModel(sh *shell) *model {
-	ctx, cancel := context.WithCancel(context.Background())
-	sh.Start(ctx)
-	return &model{
-		sh:          sh,
-		cmdInput:    textinput.New(),
-		ctx:         ctx,
-		cancelShell: cancel,
-	}
-}
-
-func (m *model) Init() tea.Cmd {
-	return tea.Batch(m.cmdInput.Focus(), m.executeCommandFull)
-}
-
-func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var (
-		cmd  tea.Cmd
-		cmds []tea.Cmd
-	)
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c":
-			m.cancelShell()
-			return m, tea.Quit
-		case "enter":
-			m.input.Reset()
-			m.viewport.SetContent("")
-			// cmds = append(cmds, tea.ClearScreen)
-			cmds = append(cmds, m.executeCommandFull)
-		}
-	case tea.WindowSizeMsg:
-		cmdHeight := lipgloss.Height(m.renderCommandLine())
-		m.viewport = viewport.New(msg.Width, msg.Height-cmdHeight*2)
-		m.viewport.KeyMap = viewportKeyMap
-		// m.viewport.HighPerformanceRendering = highPerformance
-		m.viewport.YPosition = 1
-		m.cmdInput.Width = msg.Width - 3
-		// cmds = append(cmds, viewport.Sync(m.viewport))
-	case outputWithClear:
-		m.input.Reset()
-		m.input.Write(msg)
-		// atBottom := m.viewport.AtBottom()
-		m.viewport.SetContent(m.input.String())
-		// if atBottom {
-		// 	m.viewport.GotoBottom()
-		// }
-	case []byte:
-		// m.input.Reset()
-		m.input.Write(msg)
-		atBottom := m.viewport.AtBottom()
-		m.viewport.SetContent(m.input.String())
-		if atBottom {
-			m.viewport.GotoBottom()
-		}
-
-	}
-
-	m.cmdInput, cmd = m.cmdInput.Update(msg)
-	cmds = append(cmds, cmd)
-
-	m.viewport, cmd = m.viewport.Update(msg)
-	cmds = append(cmds, cmd)
-
-	return m, tea.Batch(cmds...)
-}
-
-func (m *model) executeCommand() tea.Msg {
-	cmd := m.cmdInput.Value()
-	if strings.TrimSpace(cmd) == "" {
-		return m.echoCommand()
-	}
-
-	op := m.sh.ExecuteCommandLineByLine(m.ctx, cmd)
-	go func() {
-		for line := range op {
-			m.self.Send(line)
-		}
-	}()
-
-	return nil
-}
-
-func (m *model) echoCommand() tea.Msg {
-	op := m.sh.ListenInput(m.ctx)
-	go func() {
-		for line := range op {
-			m.self.Send(line)
-		}
-	}()
-
-	return nil
-}
-
-type outputWithClear []byte
-
-func (m *model) executeCommandFull() tea.Msg {
-	cmd := m.cmdInput.Value()
-	if strings.TrimSpace(cmd) == "" {
-		return m.echoCommand()
-	}
-
-	op := m.sh.ExecuteCommand(m.ctx, cmd)
-	go func() {
-		for line := range op {
-			m.self.Send(outputWithClear(line))
-		}
-	}()
-
-	return nil
-}
-
-func (m *model) View() string {
-	// return m.renderLine()
-	return fmt.Sprintf("%s\n%s\n%s", m.renderViewport(), m.renderLine(), m.renderCommandLine())
-}
-
-func (m *model) renderLine() string {
-	return strings.Repeat("─", m.viewport.Width)
-}
-
-func (m *model) renderViewport() string {
-	return m.viewport.View()
-}
-
-func (m *model) renderCommandLine() string {
-	return m.cmdInput.View()
-}
-
 func main() {
+	profile, err := os.Create("profile.pprof")
+	if err != nil {
+		panic(err)
+	}
+
+	defer profile.Close()
+
+	_ = pprof.StartCPUProfile(profile)
+	defer pprof.StopCPUProfile()
+
 	fstat, err := os.Stdin.Stat()
 	if err != nil {
 		panic(err)
@@ -191,9 +32,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	sh := NewShell(os.Stdin)
-	m := newModel(sh)
-
 	file, err := tea.LogToFile("debug.log", "")
 	if err != nil {
 		panic(err)
@@ -201,9 +39,29 @@ func main() {
 	_ = file.Truncate(0)
 	defer file.Close()
 
-	program := tea.NewProgram(m, tea.WithAltScreen())
-	m.self = program
+	stdout := os.Stdout
+	writeToStdout := false
+	o, _ := os.Stdout.Stat()
+	if (o.Mode() & os.ModeCharDevice) != os.ModeCharDevice {
+		tty, err := os.OpenFile("/dev/tty", os.O_RDWR, 0)
+		if err != nil {
+			panic(err) // handle error properly in production code
+		}
+		defer tty.Close()
+
+		writeToStdout = true
+		os.Stdout = tty
+	}
+
+	in := shell.NewInput(os.Stdin)
+
+	m, program := ui.NewProgram(in)
 	if _, err := program.Run(); err != nil {
 		panic(err)
 	}
+
+	if writeToStdout {
+		m.WriteOutputTo(stdout)
+	}
+
 }
